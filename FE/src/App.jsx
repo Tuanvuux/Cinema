197b3b4e4162345bfc88260b3c3640f8e03b3b57
Vanthuyen <125105464+Vanthuyen@users.noreply.github.com>
2025-03-23 09:57:55 +0700

    update

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,11 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
+import Payment from "./components/Payment";
+import SeatSelection from "./components/SeatSelection";
+import MovieSlider from "./components/MovieSlider";
+import MovieDetail from "./components/MovieDetail";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/seat-selection" element={<SeatSelection />} />
+        <Route path="/movie-detail" element={<MovieDetail />} />
       </Routes>
     </Router>
   );
