f7b46aca3ba58052e8dc8d159665ed7ba35723e4
Tuan Vu <Nguyentuanvu1410@gmail.com>
2025-04-01 02:43:20 +0700

    create login and register fix route

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,22 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserRoutes from "./routes/UserRoutes";
+import PublicRoute from "./routes/PublicRoute";
 // import PrivateRoute from "./routes/PrivateRoute";
 import AdminRoutes from "./routes/AdminRoutes";
-import Register from "./pages/Register";
-import Payment from "./components/Payment";
-import SeatSelection from "./components/SeatSelection";
-import MovieDetail from "./components/MovieDetail";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         {/* Routes cho user */}
-        <Route path="/*" element={<UserRoutes />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/seat-selection" element={<SeatSelection />} />
-        <Route path="/movie-detail" element={<MovieDetail />} />
+        <Route path="/*" element={<PublicRoute />} />
         {/* Routes cho admin - yêu cầu quyền admin */}
         <Route path="/admin/*" element={<AdminRoutes />} />
       </Routes>
