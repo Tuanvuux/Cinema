cd0d3485f6498742c47a5377a4104ffc8b139f7d
Tuan Vu <Nguyentuanvu1410@gmail.com>
2025-04-14 23:30:51 +0700

    showtime before merge

--- BE/src/main/java/com/example/be/controller/ShowtimeController.java
+++ BE/src/main/java/com/example/be/controller/ShowtimeController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/showtime")
-public class ShowtimeController {
+public class ShowTimeController {
     @Autowired
     ShowTimeService showTimeService;
     @GetMapping("")
--- BE/src/main/java/com/example/be/service/ShowTimeService.java
+++ BE/src/main/java/com/example/be/service/ShowTimeService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class ShowTimeService {
